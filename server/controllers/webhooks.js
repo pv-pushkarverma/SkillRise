ccc2209e09b5be38ed75b6cebaf419e050fda065
pv-pushkarverma <pvpushkarverma@gmail.com>
2025-04-22 13:45:36 +0530

    .

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -36,7 +36,7 @@ export const clerkWebhooks = async (req, res) => {
             //Update User
             case 'user.updated' : {
                 const userData = {
-                    email: data.email_address[0].email_address,
+                    email: data.email_addresses[0].email_address,
                     name: data.first_name + ' ' + data.last_name,
                     imageUrl: data.image_url
                 }
@@ -63,5 +63,5 @@ export const clerkWebhooks = async (req, res) => {
             message: error.message
         })
     }
-    
+
 }
