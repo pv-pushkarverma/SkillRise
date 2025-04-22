63da21f1f9a7448c0ba2567389a3a4d71b0827c7
pv-pushkarverma <pvpushkarverma@gmail.com>
2025-04-22 21:20:43 +0530

    educator routes done

--- server/controllers/educatorController.js
+++ server/controllers/educatorController.js
@@ -1,4 +1,9 @@
 import { clerkClient } from '@clerk/express'
+import Course from '../models/Course.js'
+import { v2 as cloudinary } from 'cloudinary'
+import { Purchase } from '../models/Purchase.js'
+import User from '../models/User.js'
+import { DashboardAccessOut } from 'svix'
 
 //Update role to Educator
 export const updateRoleToEducator = async (req, res) => {
@@ -22,4 +27,143 @@ export const updateRoleToEducator = async (req, res) => {
             message: error.message
         })
     }    
+}
+
+//Add New Course
+export const addCourse = async (req, res) => {
+    try {
+        const { courseData } = req.body
+        const imageFile = req.file
+        const educatorId = req.auth.userId
+
+        if(!imageFile){
+            return res.json({
+                success: false,
+                message: 'Thumbnail Not Attached'
+            })
+        }
+
+        const parsedCourseData = await JSON.parse(courseData)
+        parsedCourseData.educator = educatorId
+
+        const newCourse = await Course.create(parsedCourseData)
+
+        const imageUpload = await cloudinary.uploader.upload(imageFile.path)
+
+        newCourse.courseThumbnail = imageUpload.secure_url
+
+        await newCourse.save()
+
+        res.json({
+            success: true,
+            message: 'Course Added Succesfully'
+        })
+
+    } catch (error) {
+        res.json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+//Get All Educator Courses
+export const getEducatorCourses = async (req, res) => {
+    try {
+        const educator = req.auth.userId
+        const courses = await Course.find({educator})
+
+        res.json({
+            success: true,
+            courses
+        })
+    } catch (error) {
+        res.json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+//Get Educator Dashboard Data
+export const educatorDashboardData = async (req, res) => {
+    try {
+        const educator = req.auth.userId
+        const courses = await Course.find({educator})
+        const totalCourses = courses.length
+
+        //Get all course Ids
+        const courseIds = courses.map(course => course._id)
+
+        //Calculate total earnings 
+        const purchases = await Purchase.find({
+            courseId: {$in: courseIds},
+            status: 'completed'
+        })
+
+        const totalEarnings = purchases.reduce((sum, purchase) => sum + purchase.amount, 0)
+
+        //Collect enrolled students id with course titles
+        const enrolledStudentsData = []
+
+        for(const course of courses){
+            const students = await User.find({
+                _id: {$in: course.enrolledStudents}
+            }, 'name imageUrl')
+
+            students.forEach( student => {
+                enrolledStudentsData.push({
+                    courseTitle: course.courseTitle,
+                    student
+                })
+            })
+        }
+
+        res.json({
+            success: true,
+            dashboardData: {
+                totalEarnings,
+                enrolledStudentsData,
+                totalCourses
+            }
+        })
+
+    } catch (error) {
+        res.json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+
+//Get Enrolled Students data with Purchases
+export const getEnrolledStudentsData = async (req, res) => {
+    try {
+        const educator = req.auth.userId
+        const courses = await Course.find({educator})
+        const courseIds = courses.map( course => course._id)
+
+        const purchases = await Purchase.find({
+            courseId: {$in: courseIds},
+            status: 'completed'
+        }).populate('userId', 'name imageUrl').populate('courseId', 'courseTitle')
+
+        const enrolledStudents = purchases.map( purchase => ({
+            student: purchase.userId,
+            courseTitle: purchase.courseId.courseTitle,
+            purchaseDate: purchase.createdAt
+        }))
+
+        res.json({
+            success: true,
+            enrolledStudents
+        })
+
+    } catch (error) {
+        res.json({
+            success: false,
+            message: error.message
+        })
+    }
 }
--- server/server.js
+++ server/server.js
@@ -5,6 +5,9 @@ import connectDB from './configs/mongodb.js'
 import { clerkWebhooks } from './controllers/webhooks.js'
 import educatorRouter from './routes/educatorRoutes.js'
 import { clerkMiddleware } from '@clerk/express'
+import connectCloudinary from './configs/cloudinary.js'
+import courseRouter from './routes/courseRoute.js'
+
 
 
 const app = express()
@@ -12,6 +15,8 @@ const app = express()
 
 //Connect to Database
 await connectDB()
+await connectCloudinary()
+
 
 
 //Global Middlewares
@@ -24,6 +29,7 @@ app.use(clerkMiddleware())
 app.get('/', (req, res)=> res.send('API Working'))
 app.post('/clerk', clerkWebhooks)
 app.use('/api/educator', educatorRouter)
+app.use('/api/course', courseRouter)
 
 
 const PORT = process.env.PORT || 3000
