9a539af447d6bca0baf0a1514252a77712ffb574
pv-pushkarverma <pvpushkarverma@gmail.com>
2026-03-05 10:11:57 +0530

    Enhance Dockerfile and generatePdf.js to support Puppeteer with Chromium; add executable path configuration

--- server/utils/generatePdf.js
+++ server/utils/generatePdf.js
@@ -1,8 +1,20 @@
 import puppeteer from 'puppeteer'
+import fs from 'fs'
 
 export const generatePdfBuffer = async (htmlContent) => {
+  const configuredPath = process.env.PUPPETEER_EXECUTABLE_PATH
+  const chromiumPath = configuredPath
+    ? configuredPath
+    : fs.existsSync('/usr/bin/chromium-browser')
+      ? '/usr/bin/chromium-browser'
+      : fs.existsSync('/usr/bin/chromium')
+        ? '/usr/bin/chromium'
+        : undefined
+
   const browser = await puppeteer.launch({
     headless: 'new',
+    executablePath: chromiumPath,
+    args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage'],
   })
 
   try {
